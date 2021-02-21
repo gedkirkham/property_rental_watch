ee760ae6f20a2b3b923589b6d20f65faab14c4cc
ged <gedkirkham@protonmail.com>
2021-02-21 18:05:12 +0000

    style: update styling for create review page

--- app/reviews/forms.py
+++ app/reviews/forms.py
@@ -1,12 +1,20 @@
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 from .models import Review
 
 class ReviewForm(forms.ModelForm):
     """Form definition for Review."""
 
+    def __init__(self, *args, **kwargs):
+        super(ReviewForm, self).__init__(*args, **kwargs)
+        self.fields['desc'].label = _('Description')
+
     class Meta:
         """Meta definition for Reviewform."""
 
         model = Review
         fields = ('title', 'desc', 'rating',)
+        help_texts = {
+            'rating': _('Give a rating out of 5'),
+        }
